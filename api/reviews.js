dd1b57c9da2f9687158c9a085464dea4e217097b
teerryn <slauzinho@users.noreply.github.com>
2022-10-19 13:23:47 +0100

    Force update

--- api/reviews.js
+++ api/reviews.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+//force update
 const allowCors = (fn) => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
